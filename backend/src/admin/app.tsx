68ee5704952ac22743e7951c0b6464d8a743dcec
aniflax <aniflax@aol.com>
2026-07-24 01:36:33 +0530

    feat: add Strapi admin logo and favicon

---
+++ backend/src/admin/app.tsx
@@ -0,0 +1,16 @@
+import strapiLogo from "./strapi-logo.png";
+
+export default {
+  config: {
+    locales: [],
+    head: {
+      favicon: strapiLogo,
+    },
+    auth: {
+      logo: strapiLogo,
+    },
+    menu: {
+      logo: strapiLogo,
+    },
+  },
+};
